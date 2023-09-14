f5b4ea440ece1237d1afd2819bdac38276e552ee
Sergei Gagarin <66745577+Lavmee@users.noreply.github.com>
2023-09-14 22:27:18 +0300

    Using MR in common module.
    (Added Ios target for correct display of MR class in common module, if only JVM targets are used, the IDE does not display the MR class in the code complete)

--- shared/src/desktopMain/kotlin/main.desktop.kt
+++ shared/src/desktopMain/kotlin/main.desktop.kt
@@ -1,12 +1,7 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 
 actual fun getPlatformName(): String = "Desktop"
-
-@Composable
-actual fun getCatImage(): Painter = painterResource("images/background_cat.jpg")
 
 @Composable fun MainView() = App()
 
